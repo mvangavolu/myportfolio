6f0fce7f0a23e46252bc727173f65787cd7a595b
mvangavolu <52643537+mvangavolu@users.noreply.github.com>
2024-07-26 09:31:48 -0500

    content change

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -16,7 +16,7 @@ const Footer = ({}) => {
             <h1 className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl text-bold">
               TOGETHER
             </h1>
-            <Button type="primary">Schedule a call</Button>
+            <Button onClick={() => window.open("mailto:hello@chetanverma.com")}>Schedule a call</Button>
             <div className="mt-10">
               <Socials />
             </div>
